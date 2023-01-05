12fc4b6f9cae9b103aa991ffc7b44728c7329a25
Junior Fonseca <edinaldoffjunior@gmail.com>
2023-01-04 23:42:41 -0300

    .

--- tentando_features.R
+++ tentando_features.R
@@ -9,4 +9,23 @@ library(ggplot2)
 jogos <- read.csv2('csv/df.csv') %>% dplyr::select(-X)
 jogadores <- read.csv2('csv/jogadores.csv') %>% dplyr::select(-X)
 
-ggplot(jogos, aes(x, y))
+
+# Esboços:
+
+ggplot(data = jogadores)
+geom_point(data = jogos)
+
+jogadores %>% 
+  group_by(R) %>% 
+  summarise(ACS = mean(ACS, na.rm = TRUE)) %>% 
+  ggplot() +
+  geom_line(aes(x = R, y = ACS))
+
+jogos %>% 
+  group_by(time2R) %>% 
+  ggplot() +
+  geom_line(aes(x = time2R, y = time1R)) +
+  geom_point(aes(x = time2R, y = time1R))
+
+
+
