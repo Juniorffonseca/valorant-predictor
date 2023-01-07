971bb020e74f3f0d4dc0af4ff19dbee3d89be240
Junior Fonseca <edinaldoffjunior@gmail.com>
2023-01-07 14:38:19 -0300

    script_simplificado

--- 02_jogos.R
+++ 02_jogos.R
@@ -10,50 +10,55 @@ library(reshape2)
 library(readr)
 library(purrr)
 
+# Criando variável páginas e criando variável 'p' que será a parte final do url (o número da página) -------
 paginas <- ''
 p <- 1
 
+# Criando um laço for que armazenará o url de cada página dentro da variável paginas -----------------------
 for (i in 1:33){
   paginas[p] <- paste('https://www.vlr.gg/matches/results/?page=', p, sep = '')
   p = p + 1
 }
 
+# Variável partidas e variável c ---------------------------------------------------------------------------
 c <- 1
-matchs <- 'a'
+partidas <- 'a'
 
+# Função que retorna o url de cada partida -----------------------------------------------------------------
 funcaoPagina <- function(pagina){
   
-  matchs <- read_html(pagina) %>% 
-    html_nodes('a') %>% html_attr('href')
+  partidas <- read_html(pagina) %>% 
+    html_nodes('a') %>% html_attr('href') # Nessa parte ele pega todos os urls que estão contidos na página.
   
-  matchs <- matchs[15:64]
+  partidas <- partidas[15:64] # Aqui é separado os urls que são efetivamente de partidas.
   
   n <- 1
   
-  for (i in matchs){
-    matchs[n] <- paste('https://www.vlr.gg', matchs[n], sep = '')
+  for (i in partidas){
+    partidas[n] <- paste('https://www.vlr.gg', partidas[n], sep = '') # Salvando urls dentro da variável partida
     n = n + 1
-    
   }
   
-  return(matchs)
+  return(partidas)
   
 }
 
+# Criando f e uma lista que receberá todos os returns da funcaoPagina (url de cada partida) ----------------
 f <- 1
 a <- list()
 
+# Executando um for que fará a iteração da funcaoPagina todas as vezes necessárias -------------------------
 for (i in paginas){
   a[[length(a)+1]] = funcaoPagina(paginas[f])
   f = f + 1
-
 }
 
-rm (c, f, i, p, matchs, paginas)
+# Fazendo unlist de 'a' e criando 'm' e 'dff' (lista que receberá todos os dados dos jogos) ----------------
 m <- 1
 a <- unlist(a)
 dff <- list()
 
+# Função que vai catalogar e retornar os dados das partidas -------------------------------------------------
 catalogarporUrl <- function (string){
   tryCatch(
     
@@ -143,6 +148,7 @@ catalogarporUrl <- function (string){
   
 }
 
+# Iteração para catalogar todos os jogos contidos nos urls armazenados --------------------------------------
 for (i in a){
   tryCatch({
   dff[[length(dff)+1]] <- catalogarporUrl(a[m])
@@ -150,8 +156,9 @@ for (i in a){
   }, error = function(e){cat('error:', conditionMessage(e), '\n')})
 }
 
+# Passando os dados recebidos para um dataframe mais organizado --------------------------------------------
 dff <- dff %>% map_df(as_tibble)
 
+# Exportando como csv --------------------------------------------------------------------------------------
 write.csv2(dff, 'csv/df.csv')
-
 
