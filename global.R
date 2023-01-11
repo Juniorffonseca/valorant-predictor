1f03a03b157b26c47b1d5b51156c65b827c242b6
Junior Fonseca <edinaldoffjunior@gmail.com>
2023-01-11 17:11:12 -0300

    .

--- global.R
+++ global.R
@@ -21,7 +21,16 @@ source('server.R')
 
 # Carregando a rede neural e o dataframe Jogadores
 load(file = "model_nnet.rda")
-dados_gerais <- read.csv2('csv/jogadores.csv')
+
+# Coletando os dados dos jogadores de forma atualizada
+link <- "https://www.vlr.gg/stats/?event_group_id=all&event_id=all&region=all&country=all&min_rounds=50&min_rating=1550&agent=all&map_id=all&timespan=all"
+
+players <- read_html(link) %>% 
+  html_node("table") %>% 
+  html_table() %>% 
+  separate(Player, into = c("Player", "Team"), sep = "\\s+", extra = "merge") %>% 
+  select('Player', 'Team', 'R', 'ACS', 'K:D', 'KAST', 'ADR') %>% 
+  as.data.frame()
 
 # Criando o app
 shinyApp(ui = ui, server = server)
--- server.R
+++ server.R
@@ -4,10 +4,10 @@ server <- function(input, output) {
   previsaoInput <- reactive({
     
     # Arrumando as colunas -------------------------------------------------------------------------------------
-    dados_gerais <- dplyr::select(dados_gerais, Player, R, ACS, K.D, KAST, ADR)
-    row.names(dados_gerais) <- make.names(dados_gerais[,1], unique = T)
-    dados_gerais <- dplyr::select(dados_gerais, -Player)
-    dados_gerais$KAST <- parse_number(dados_gerais$KAST)
+    players <- dplyr::select(players, Player, R, ACS, 'K:D', KAST, ADR)
+    row.names(players) <- make.names(players[,1], unique = T)
+    players <- dplyr::select(players, -Player)
+    players$KAST <- parse_number(players$KAST)
     
     # Pegando os dados no link da partida ----------------------------------------------------------------------
     info <- read_html(input$texturl) %>% 
@@ -37,20 +37,20 @@ server <- function(input, output) {
     # Time A
     #timeA = c('nome1', 'nome2', 'nome3', 'nome4', 'nome5') # se preferir passar de forma manual
     timeA <- paste0('\\b', timeA, '\\b') 
-    dados_gerais$timeA <- ifelse(grepl(paste(timeA, collapse = '|'), rownames(dados_gerais), useBytes = T), 1, 0)
-    dados_gerais['nobody.1',]$timeA <- 0
-    dados_gerais['Laz.1',]$timeA <- 0 
+    players$timeA <- ifelse(grepl(paste(timeA, collapse = '|'), rownames(players), useBytes = T), 1, 0)
+    players['nobody.1',]$timeA <- 0
+    players['Laz.1',]$timeA <- 0 
     
     # Time B
     #timeB = c('nome1', 'nome2', 'nome3', 'nome4', 'nome5') # se preferir passar de forma manual
     timeB <- paste0('\\b', timeB, '\\b') 
-    dados_gerais$timeB <- ifelse(grepl(paste(timeB, collapse = '|'), rownames(dados_gerais), useBytes = T), 1, 0)
-    dados_gerais['Shiro.1',]$timeB<- 0
+    players$timeB <- ifelse(grepl(paste(timeB, collapse = '|'), rownames(players), useBytes = T), 1, 0)
+    players['Shiro.1',]$timeB<- 0
     
-    timeA_df <- filter(dados_gerais, dados_gerais$timeA == 1)
-    timeA_df <- dplyr::select(timeA_df, R, ACS, K.D, KAST, ADR)
-    timeB_df <- filter(dados_gerais, dados_gerais$timeB == 1) 
-    timeB_df <- dplyr::select(timeB_df, R, ACS, K.D, KAST, ADR)
+    timeA_df <- filter(players, players$timeA == 1)
+    timeA_df <- dplyr::select(timeA_df, R, ACS, 'K:D', KAST, ADR)
+    timeB_df <- filter(players, players$timeB == 1) 
+    timeB_df <- dplyr::select(timeB_df, R, ACS, 'K:D', KAST, ADR)
     
     if(nrow(timeA_df) == 5 && nrow(timeB_df) == 5){
       
@@ -58,12 +58,12 @@ server <- function(input, output) {
       timeA_R <- mean(timeA_df$R)
       timeA_ACS <- mean(timeA_df$ACS)
       timeA_KAST <- mean(timeA_df$KAST)
-      timeA_KD <- mean(timeA_df$K.D)
+      timeA_KD <- mean(timeA_df$'K:D')
       timeA_ADR <- mean(timeA_df$ADR)
       timeB_R <- mean(timeB_df$R)
       timeB_ACS <- mean(timeB_df$ACS)
       timeB_KAST <- mean(timeB_df$KAST)
-      timeB_KD <- mean(timeB_df$K.D)
+      timeB_KD <- mean(timeB_df$'K:D')
       timeB_ADR <- mean(timeB_df$ADR)
       
       partida <- c(timeA_R, timeB_R, timeA_ACS, timeB_ACS, timeA_KAST, timeB_KAST, timeA_KD, timeB_KD,
