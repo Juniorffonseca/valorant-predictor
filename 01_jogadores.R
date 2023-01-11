1d4b3a10c6a6ed495fca6c642fb65fd3de8e7d26
Junior Fonseca <edinaldoffjunior@gmail.com>
2023-01-11 16:52:54 -0300

    script_simplificado

--- 01_jogadores.R
+++ 01_jogadores.R
@@ -7,13 +7,11 @@ library(httr)
 # Armazenando a url em uma variável -----------------------------------------------------------------------
 link <- "https://www.vlr.gg/stats/?event_group_id=all&event_id=all&region=all&country=all&min_rounds=50&min_rating=1550&agent=all&map_id=all&timespan=all"
 
-# Pegando os Dados no link -------------------------------------------------------------------------------
+# Pegando os Dados no link e separando time e jogador em duas colunas -------------------------------------
 players <- read_html(link) %>% 
   html_node("table") %>% 
-  html_table()
-
-# Separando time e jogador em duas colunas --------------------------------------------------------------
-players <- separate(players, Player, into = c("Player", "Team"), sep = "\\s+", extra = "merge") %>% 
+  html_table() %>% 
+  separate(Player, into = c("Player", "Team"), sep = "\\s+", extra = "merge") %>% 
   select('Player', 'Team', 'R', 'ACS', 'K:D', 'KAST', 'ADR')
 
 # Exportando como csv -------------------------------------------------------------------------------------
