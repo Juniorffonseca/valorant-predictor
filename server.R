18a9b78007f44f5a04af9f18e082acc56e7d16d9
Junior Fonseca <edinaldoffjunior@gmail.com>
2023-01-16 18:26:12 -0300

    .

--- server.R
+++ server.R
@@ -38,14 +38,11 @@ server <- function(input, output) {
     #timeA = c('nome1', 'nome2', 'nome3', 'nome4', 'nome5') # se preferir passar de forma manual
     timeA <- paste0('\\b', timeA, '\\b') 
     players$timeA <- ifelse(grepl(paste(timeA, collapse = '|'), rownames(players), useBytes = T), 1, 0)
-    players['nobody.1',]$timeA <- 0
-    players['Laz.1',]$timeA <- 0 
     
     # Time B
     #timeB = c('nome1', 'nome2', 'nome3', 'nome4', 'nome5') # se preferir passar de forma manual
     timeB <- paste0('\\b', timeB, '\\b') 
     players$timeB <- ifelse(grepl(paste(timeB, collapse = '|'), rownames(players), useBytes = T), 1, 0)
-    players['Shiro.1',]$timeB<- 0
     
     timeA_df <- filter(players, players$timeA == 1)
     timeA_df <- dplyr::select(timeA_df, R, ACS, 'K:D', KAST, ADR)
@@ -134,12 +131,129 @@ server <- function(input, output) {
     }
   })
 
+  previsaoInput2 <- reactive({
+    
+    # Arrumando as colunas -------------------------------------------------------------------------------------
+    players <- dplyr::select(players, Player, R, ACS, 'K:D', KAST, ADR)
+    row.names(players) <- make.names(players[,1], unique = T)
+    players <- dplyr::select(players, -Player)
+    players$KAST <- parse_number(players$KAST)
+    
+    # Time A
+    timeA = c(input$t1j1, input$t1j2, input$t1j3, input$t1j4, input$t1j5)
+    timeA <- paste0('\\b', timeA, '\\b') 
+    players$timeA <- ifelse(grepl(paste(timeA, collapse = '|'), rownames(players), useBytes = T), 1, 0)
+    
+    # Time B
+    timeB = c(input$t2j1, input$t2j2, input$t2j3, input$t2j4, input$t2j5)
+    timeB <- paste0('\\b', timeB, '\\b') 
+    players$timeB <- ifelse(grepl(paste(timeB, collapse = '|'), rownames(players), useBytes = T), 1, 0)
+    
+    timeA_df <- filter(players, players$timeA == 1)
+    timeA_df <- dplyr::select(timeA_df, R, ACS, 'K:D', KAST, ADR)
+    timeB_df <- filter(players, players$timeB == 1) 
+    timeB_df <- dplyr::select(timeB_df, R, ACS, 'K:D', KAST, ADR)
+    
+    if(nrow(timeA_df) == 5 && nrow(timeB_df) == 5){
+      
+      # Médias
+      timeA_R <- mean(timeA_df$R)
+      timeA_ACS <- mean(timeA_df$ACS)
+      timeA_KAST <- mean(timeA_df$KAST)
+      timeA_KD <- mean(timeA_df$'K:D')
+      timeA_ADR <- mean(timeA_df$ADR)
+      timeB_R <- mean(timeB_df$R)
+      timeB_ACS <- mean(timeB_df$ACS)
+      timeB_KAST <- mean(timeB_df$KAST)
+      timeB_KD <- mean(timeB_df$'K:D')
+      timeB_ADR <- mean(timeB_df$ADR)
+      
+      partida <- c(timeA_R, timeB_R, timeA_ACS, timeB_ACS, timeA_KAST, timeB_KAST, timeA_KD, timeB_KD,
+                   timeA_ADR, timeB_ADR)
+      
+      jogos_scale <- read.csv2('csv/partidas.csv') %>% select(-X, -ganhador)
+      
+      jogos_scale <- rbind(jogos_scale, partida)
+      
+      jogos_scale <- scale(jogos_scale)
+      
+      partida <- jogos_scale[814,]
+      
+      partida <- t(partida)
+      
+      partida <- as.data.frame(partida)
+      
+      colnames(partida) <- c('time1R', 'time2R', 'time1ACS', 'time2ACS', 'time1KAST', 'time2KAST', 'time1KD', 'time2KD',
+                             'time1ADR', 'time2ADR')
+      
+      previsao <- compute(n, partida)
+      
+      previsao <- previsao$net.result[1]
+      
+      partida_reversa <- partida
+      
+      partida_reversa$time1R <- partida$time2R
+      partida_reversa$time2R <- partida$time1R
+      partida_reversa$time1ACS <- partida$time2ACS
+      partida_reversa$time2ACS <- partida$time1ACS
+      partida_reversa$time1KAST <- partida$time2KAST
+      partida_reversa$time2KAST <- partida$time1KAST
+      partida_reversa$time1KD <- partida$time2KD
+      partida_reversa$time2KD <- partida$time1KD
+      partida_reversa$time1ADR <- partida$time2ADR
+      partida_reversa$time2ADR <- partida$time1ADR
+      
+      previsao2 <- compute(n, partida_reversa)
+      
+      previsao2 <- previsao2$net.result[1]
+      
+      a <- previsao
+      b <- previsao2
+      
+      transforma_positivo <- function (x){
+        y = atan(x) + pi/2
+        return (y)
+      }
+      
+      transforma_probabilidade <- function (y, x){
+        z = y / (y + x)
+        w = x / (x + y)
+        c = as.matrix(c(z,w))
+        return(c)
+      }
+      
+      a <- transforma_positivo(a)
+      b <- transforma_positivo(b)
+      previsao <- transforma_probabilidade(a,b)
+      
+      previsao <- previsao * 100
+      
+      return(previsao)
+      
+    }
+    else{
+      return('Não foi possível fazer a análise, provavelmente dados de um ou mais jogadores estavam faltantes no site vlr')
+      return(timeA)
+    }
+  })
+  
   output$txtout <- renderText({
     if (input$submitbutton>0) { 
       if (is.numeric(previsaoInput())){
-      paste(round(previsaoInput(), 2), '%', '')}
+        paste(round(previsaoInput(), 2), '%', '')}
       else {
         paste(previsaoInput())}} 
+    else {
+      return("A rede neural está pronta para calcular as probabilidades.")
+    }
+  })
+  
+  output$txtout2 <- renderText({
+    if (input$submitbutton2>0) { 
+      if (is.numeric(previsaoInput2())){
+        paste(round(previsaoInput2(), 2), '%', '')}
+      else {
+        paste(previsaoInput2())}} 
     else {
       return("A rede neural está pronta para calcular as probabilidades.")
     }
--- ui.R
+++ ui.R
@@ -1,20 +1,34 @@
 # Define UI
-ui <- fluidPage(theme = shinytheme("cyborg"),
+ui <- fluidPage(theme = shinytheme("superhero"),
                 navbarPage(
                   "Valorant",
-                  tabPanel("Prever partidas",
+                  tabPanel("Por link",
                            sidebarPanel(
                              textInput("texturl", "Url da partida:", ""),
                              actionButton('submitbutton', 'Prever', 
                                           class = 'btn btn-primary'),
-                             
-                             
-                           ),
-                           mainPanel(
                              h5("Probabilidade de vitória de cada time:"),
-                             verbatimTextOutput("txtout"),
-                             
-                           )
+                             verbatimTextOutput("txtout")
+                             )
+                           ),
+                  tabPanel(
+                    'Por nicks',
+                    sidebarPanel(
+                      textInput('t1j1', 'Jogador1 (time1):', ''),
+                      textInput('t1j2', 'Jogador2 (time1):', ''),
+                      textInput('t1j3', 'Jogador3 (time1):', ''),
+                      textInput('t1j4', 'Jogador4 (time1):', ''),
+                      textInput('t1j5', 'Jogador5 (time1):', ''),
+                      textInput('t2j1', 'Jogador1 (time2):', ''),
+                      textInput('t2j2', 'Jogador2 (time2):', ''),
+                      textInput('t2j3', 'Jogador3 (time2):', ''),
+                      textInput('t2j4', 'Jogador4 (time2):', ''),
+                      textInput('t2j5', 'Jogador5 (time2):', ''),
+                      actionButton('submitbutton2', 'Prever', 
+                                   class = 'btn btn-primary'),
+                      h5("Probabilidade de vitória de cada time:"),
+                      verbatimTextOutput("txtout2")
+                      )
                   )
                 )
 )
