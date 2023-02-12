3b8a83df6abb0ab16e39f10c232bff0a7b7c5d49
Junior Fonseca <edinaldoffjunior@gmail.com>
2023-02-12 20:32:28 -0300

    .

--- global.R
+++ global.R
@@ -1,4 +1,6 @@
 # Carregando pacotes
+library(devtools)
+install_github("Juniorffonseca/r-pacote-valorant")
 library(shiny)
 library(shinythemes)
 library(dplyr)
@@ -14,6 +16,7 @@ library(data.table)
 library(readr)
 library(ggplot2)
 library(DT)
+library(valorant)
 
 # Carregando ui e server
 source('ui.R')
@@ -21,16 +24,6 @@ source('server.R')
 
 # Carregando a rede neural e o dataframe Jogadores
 load(file = "model_nnet.rda")
-
-# Coletando os dados dos jogadores de forma atualizada
-link <- "https://www.vlr.gg/stats/?event_group_id=all&event_id=all&region=all&country=all&min_rounds=50&min_rating=1550&agent=all&map_id=all&timespan=all"
-
-players <- read_html(link) %>% 
-  html_node("table") %>% 
-  html_table() %>% 
-  separate(Player, into = c("Player", "Team"), sep = "\\s+", extra = "merge") %>% 
-  select('Player', 'Team', 'R', 'ACS', 'K:D', 'KAST', 'ADR') %>% 
-  as.data.frame()
 
 # Criando o app
 shinyApp(ui = ui, server = server)
