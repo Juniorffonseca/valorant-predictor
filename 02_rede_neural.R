69e13d7d8e652ea1b84d8ffc85a8a489698bbd25
Junior Fonseca <edinaldoffjunior@gmail.com>
2023-02-17 21:42:41 -0300

    .

--- 02_rede_neural.R
+++ 02_rede_neural.R
@@ -12,7 +12,9 @@ library(ggplot2)
 
 # Carregando o dataframe -----------------------------------------------------------------------------------
 jogos <- read.csv2('csv/partidas.csv') %>% dplyr::select(-X)
-jogos <- read.csv2('csv/partidas_teste.csv') %>% dplyr::select(-X)
+jogos <- read.csv2('csv/partidas_teste.csv') %>% dplyr::select(-X, -time1K, -time2K, -time1D, -time2D,
+                                                               -time1KPR, -time2KPR, -time1APR, -time2APR,
+                                                               -time1FKPR, -time2FKPR, -time1FDPR, -time2FDPR)
 
 # Criando dataframes de teste e validação -----------------------------------------------------------------
 set.seed(1)
@@ -38,7 +40,7 @@ test_data$ganhador <- as.factor(test_data$ganhador)
 # Modelando a rede neural ---------------------------------------------------------------------------------
 n <- neuralnet(ganhador == 1 ~ .,
                data = training_data,
-               hidden = c(30),
+               hidden = c(20, 20),
                err.fct = "sse",
                linear.output = F,
                threshold = 0.1,
@@ -79,10 +81,10 @@ acharseed <- function(seed){
   
   n <- neuralnet(ganhador == 1 ~ .,
                  data = training_data,
-                 hidden = c(30),
+                 hidden = c(20, 20),
                  err.fct = "sse",
                  linear.output = F,
-                 threshold = 0.1,
+                 threshold = 1,
                  lifesign = 'minimal',
                  rep = 1,
                  algorithm = 'rprop-',
@@ -101,13 +103,13 @@ acharseed <- function(seed){
 
 s <- 1
 
-while ( i < 0.78) {
+while ( i < 0.8) {
   acharseed(s)
   s <- s + 1
 }
 
 # Atualizando a seed para achar a melhor neuralnetwork -------------------------------------------------------
-set.seed(61) #4 #59
+set.seed(27741) #4 #59
 inp <- sample(2, nrow(jogos), replace = TRUE, prob = c(0.7, 0.3))
 training_data <- jogos[inp==1, ]
 test_data <- jogos[inp==2, ]
@@ -131,32 +133,35 @@ predictVstest <- cbind(test_data, Predict$net.result)
 
 acharnn <- function(){
   
-  n <- neuralnet(ganhador == 1 ~ .,
+  n <<- neuralnet(ganhador == 1 ~ .,
                  data = training_data,
-                 hidden = c(30),
+                 hidden = c(20, 20),
                  err.fct = "sse",
                  linear.output = F,
-                 threshold = 0.1,
+                 threshold = 1,
                  lifesign = 'minimal',
                  rep = 1,
                  algorithm = 'rprop-',
                  stepmax = 10000)
   
-  Predict = compute(n, test_data)
+  Predict <<- compute(n, test_data)
   
-  nn2 <- ifelse(Predict$net.result[,1]>mean(Predict$net.result),1,0)
+  nn2 <<- ifelse(Predict$net.result[,1]>mean(Predict$net.result),1,0)
   
-  predictVstest <- cbind(test_data, Predict$net.result)
+  predictVstest <<- cbind(test_data, Predict$net.result)
   i <<- sum(predictVstest$ganhador == nn2)/ nrow(test_data)
   print(i)
   
+  
+  
 }
 
-while ( i < 0.80) {
+while (i < 0.84) {
   acharnn()
 }
 
-save(n, file ='rede_neural.rda')
+#save(n, file ='rede_neural.rda')
+save(n, file='rede_neural_teste.rda')
 # Matriz de confusão ---------------------------------------------------------------------------------------
 jogos <- read.csv2('csv/partidas.csv') %>% dplyr::select(-X)
 set.seed(5)
