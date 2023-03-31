e2ff211303917fc11024d98e5843b32b2bae8727
Junior Fonseca <edinaldoffjunior@gmail.com>
2023-03-31 00:19:02 -0300

    .

---
+++ acuracia_total.R
@@ -0,0 +1,42 @@
+# Instalando pacotes (se necessário) e carregando ----------------------------------------------------------
+library(devtools)
+install_github('Juniorffonseca/r-pacote-valorant')
+library(caret)
+library(dplyr)
+library(tidyr)
+library(rvest)
+library(rsample)
+library(readr)
+library(quantmod)
+library(httr)
+library(tibble)
+library(stringr)
+library(neuralnet)
+library(nnet)
+library(caret)
+library(ggplot2)
+library(ModelMetrics)
+library(beepr)
+library(purrr)
+library(plotly)
+library(pROC)
+library(ROCR)
+library(kableExtra)
+library(valorant)
+
+
+# Carregando partidas diarias e unindo em um df ------------------------------------------------------------
+datas <- seq(as.Date('2023-03-14'), Sys.Date() - 1, by = 'day')
+nomes_arquivos <- paste0('csv/previsao_diaria/', format(datas, '%Y-%m-%d'), '_acuracia.csv')
+
+acuracia <- list()
+
+for (arquivo in nomes_arquivos) {
+  acuracia[[arquivo]] <- possibly(read.csv2, otherwise = NULL)(arquivo)
+}
+
+acuracia <- bind_rows(acuracia) %>% select(-X)
+
+acertos <- sum(acuracia$acertos)
+erros <- sum(acuracia$erros)
+print(acuracia_total <- acertos/(acertos+erros))
