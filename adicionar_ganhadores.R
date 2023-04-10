45b404517a4eae78335e48f155fc32571b10786c
Junior Fonseca <edinaldoffjunior@gmail.com>
2023-04-09 23:17:59 -0300

    .

--- adicionar_ganhadores.R
+++ adicionar_ganhadores.R
@@ -17,15 +17,24 @@ setwd('C:/Users/anonb/Documents/TCC_Pós/Scripts')
 nome_arquivo_urls <- paste(Sys.Date() - 1, '_urls.csv', sep = '')
 nome_arquivo_partidas <- paste(Sys.Date() - 1, '_partidas.csv', sep = '')
 
-b <- read.csv2(paste('csv/catalogacao_diaria/', nome_arquivo_urls, sep = '')) %>% select(-X) %>% unlist()
+b <- read.csv2(paste('csv/catalogacao_diaria/', nome_arquivo_urls, sep = '')) %>% select(-X)
 dff <- read.csv2(paste('csv/catalogacao_diaria/', nome_arquivo_partidas, sep = '')) %>% select(-X)
 
 ganhador <- '' %>% .[0]
 
-for (i in b){
+for (i in b[,]){
   ganhador[length(ganhador)+1] <- get_Ganhadores(i)
 }
 
 dff$ganhador <- ganhador
 
+dff <- dff[!dff$ganhador %in% "empate", ]
+
+b <- b[as.numeric(rownames(dff)),]
+
+rownames(dff) <- NULL
+rownames(b) <- NULL
+
+
 write.csv2(dff, paste('csv/catalogacao_diaria/', nome_arquivo_partidas, sep = ''))
+write.csv2(b, paste('csv/catalogacao_diaria/', nome_arquivo_urls, sep = ''))
