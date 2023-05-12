e22053badf376b4e7799f0b5ffd7f476e6162787
Junior Fonseca <edinaldoffjunior@gmail.com>
2023-05-12 00:14:43 -0300

    .

--- h2h_pastmatchs.R
+++ h2h_pastmatchs.R
@@ -34,15 +34,32 @@ row.names(urls) <- NULL
 
 url_teste <- urls[1,]
 
-# H2H
+# H2H -------------------------------------------------------------------------------------------------------
 x <- read_html(url_teste) %>% html_nodes('div.match-h2h-matches-score') %>% html_text() %>%
-  str_replace_all('\n', ' ') %>% str_replace_all('\t', '')
-x
+  str_replace_all('\n', ' ') %>% str_replace_all('\t', '') %>%
+  str_match("^\\s*(\\d+)\\s*(\\d+)\\s*$")
 
-# Past Matchs
-z <- read_html(url_teste) %>% html_nodes('div.wf-card.mod-dark.match-histories.mod-first.mod-loss') %>%
+x <- x[,-1] 
+
+t1 <- sum(as.numeric(x[,1]))
+t2 <- sum(as.numeric(x[,2]))
+h2h <- cbind(t1, t2)
+## Finalizado praticamente, agr só preciso colocar para iterar em todos os links ----------------------------
+
+# Past Matchs -----------------------------------------------------------------------------------------------
+z <- read_html(url_teste) %>% html_nodes('div.match-histories-item-result span') %>%
   html_text() %>% str_replace_all('\n', ' ') %>% str_replace_all('\t', '')
 
-z
+z <- as.numeric(z)
 
-# Parei aqui
+wins_t1 <- sum(z[1], z[3], z[5], z[7], z[9])
+loses_t1 <- sum(z[2], z[4], z[6], z[8], z[10])
+profit_t1 <- (wins_t1 - loses_t1)
+
+wins_t2 <- sum(z[11], z[13], z[15], z[17], z[19])
+loses_t2 <- sum(z[12], z[14], z[16], z[18], z[20])
+profit_t2 <- (wins_t2 - loses_t2)
+
+past_matches <- cbind(profit_t1, profit_t2) 
+## Finalizado praticamente, agr só preciso colocar para iterar em todos os links ----------------------------
+
